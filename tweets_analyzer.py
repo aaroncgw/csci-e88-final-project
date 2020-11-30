4c69ef4cc5b37e6f6593df8fa8ab8d657d7d65d6
aaroncgw <guc074@g.harvard.edu>
2020-11-30 17:23:24 -0500

    feat: publish sentiment back to Kafka

--- tweets_analyzer.py
+++ tweets_analyzer.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import pickle
 from json import dumps
 
 from pyspark import SparkConf, SparkContext
@@ -10,72 +11,51 @@ from kafka import KafkaProducer, KafkaAdminClient
 from kafka.admin import NewTopic
 
 
-
 class TweetStreamAnalyzer():
     def __init__(self, upstreamTopic, posDict, negDict):
         self.upstreamTopic = upstreamTopic
         self.posDict = posDict
         self.negDict = negDict
+        self.kafka_topic = "TweetStreamAnalyzer"
+        self.kafka_broker = "localhost:9092"
+
 
     def run(self):
         try:
-            admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
-            topic = NewTopic(name="TweetStreamAnalyzer",
+            admin = KafkaAdminClient(bootstrap_servers=self.kafka_broker)
+            topic = NewTopic(name=self.kafka_topic,
                              num_partitions=1,
                              replication_factor=1)
             admin.create_topics([topic])
         except Exception as e:
             print("Create Kafka topic failed: " + str(e))
 
-        producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+        producer = KafkaProducer(bootstrap_servers=[self.kafka_broker],
                                  value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-        conf = SparkConf().setMaster("local[2]").setAppName("Streamer")
+        conf = SparkConf().setMaster("local[2]").setAppName("TweetStreamAnalyzer")
         sc = SparkContext(conf=conf)
-
-        # Creating a streaming context with batch interval of 10 sec
         ssc = StreamingContext(sc, 10)
-        ssc.checkpoint("checkpoint")
 
         kafkaStream = KafkaUtils.createDirectStream(
-            ssc, topics=["TweetStreamListener"], kafkaParams={"metadata.broker.list": 'localhost:9092'})
+            ssc, topics=[self.upstreamTopic], kafkaParams={"metadata.broker.list": self.kafka_broker})
         tweets = kafkaStream.map(lambda x: x[1])
-
-        # Each element of tweets will be the text of a tweet.
-        # We keep track of a running total counts and print it at every time step.
         words = tweets.flatMap(lambda line: line.split(" "))
         positive = words.map(lambda word: ('Positive', 1) if word in self.posDict else ('Positive', 0))
         negative = words.map(lambda word: ('Negative', 1) if word in self.negDict else ('Negative', 0))
         allSentiments = positive.union(negative)
         sentimentCounts = allSentiments.reduceByKey(lambda x, y: x + y)
 
-        def updateFunction(newValues, runningCount):
-            if runningCount is None:
-                runningCount = 0
-            return sum(newValues, runningCount)
-
-        runningSentimentCounts = sentimentCounts.updateStateByKey(updateFunction)
-        runningSentimentCounts.pprint(5)
-
-
         def handler(message):
             records = message.collect()
             for record in records:
-                producer.send('TweetStreamAnalyzer', str(record))
+                producer.send(self.kafka_topic, str(record))
                 producer.flush()
 
-        #kafkaStream.foreachRDD(handler)
+        sentimentCounts.foreachRDD(handler)
 
-        # # The counts variable hold the word counts for all time steps
-        # counts = []
-        # sentimentCounts.foreachRDD(lambda t, rdd: counts.append(rdd.collect()))
-
-
-
-        # Start the computation
-        duration = 100
         ssc.start()
-        ssc.awaitTerminationOrTimeout(duration)
+        ssc.awaitTermination()
         ssc.stop(stopGraceFully=True)
 
 
@@ -85,38 +65,15 @@ def main():
 
     args = parser.parse_args()
 
-    def load_wordlist(filename):
-        """
-        This function returns a list or set of words from the given filename.
-        """
-        words = {}
-        f = open(filename, 'r')
-        text = f.read()
-        text = text.split('\n')
-        for line in text:
-            words[line] = 1
-        f.close()
-        return words
-
-
     LOCAL_ROOT = os.path.abspath("data") + os.sep
+    with open(LOCAL_ROOT + 'sentiment_pos_dict.pickle', 'rb') as handle:
+        posDict = pickle.load(handle)
 
-    pwords = load_wordlist(LOCAL_ROOT + "positive.txt")
-    nwords = load_wordlist(LOCAL_ROOT + "negative.txt")
+    with open(LOCAL_ROOT + 'sentiment_neg_dict.pickle', 'rb') as handle:
+        negDict = pickle.load(handle)
 
-    # LOCAL_ROOT = os.path.abspath("data") + os.sep
-    # with open(LOCAL_ROOT + 'sentiment_pos_dict.pickle', 'rb') as handle:
-    #     posDict = pickle.load(handle)
-    #
-    # with open(LOCAL_ROOT + 'sentiment_neg_dict.pickle', 'rb') as handle:
-    #     negDict = pickle.load(handle)
-
-
-    tweetsStreamAnalyzer = TweetStreamAnalyzer(upstreamTopic=args.topic, posDict=pwords, negDict=nwords)
+    tweetsStreamAnalyzer = TweetStreamAnalyzer(upstreamTopic=args.topic, posDict=posDict, negDict=negDict)
     tweetsStreamAnalyzer.run()
-
-
-
 
 
 if __name__ == "__main__":
--- tweets_listener.py
+++ tweets_listener.py
@@ -11,18 +11,19 @@ from kafka import KafkaProducer, KafkaAdminClient
 from kafka.admin import NewTopic
 
 class TweetStreamListener(tweepy.StreamListener):
-    def __init__(self, tweepy_api, kafka_topic):
+    def __init__(self, tweepy_api):
         super(tweepy.StreamListener, self).__init__()
         self.api = tweepy_api
-        self.kafka_topic = kafka_topic
+        self.kafka_topic = "TweetStreamListener"
+        self.kafka_broker = "localhost:9092"
 
     def on_connect(self):
-        self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+        self.producer = KafkaProducer(bootstrap_servers=[self.kafka_broker],
                                       value_serializer=lambda x: dumps(x).encode('utf-8'))
 
         try:
-            admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
-            topic = NewTopic(name="TweetStreamListener",
+            admin = KafkaAdminClient(bootstrap_servers=self.kafka_broker)
+            topic = NewTopic(name=self.kafka_topic,
                              num_partitions=1,
                              replication_factor=1)
             admin.create_topics([topic])
@@ -33,13 +34,12 @@ class TweetStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         """ This method is called whenever new data arrives from live stream.
                 We asynchronously push this data to kafka queue"""
-
         now = datetime.datetime.now()
         print(now.strftime("%Y-%m-%d %H:%M:%S"))
         print(status.text)
 
         try:
-            self.producer.send("TweetStreamListener", status.text)
+            self.producer.send(self.kafka_topic, status.text)
         except Exception as e:
             print(e)
             return False
@@ -57,33 +57,29 @@ class TweetStreamListener(tweepy.StreamListener):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--topic", default="tweepy-stream")
-
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--topic", default="TweetStreamListener")
+    #
+    # args = parser.parse_args()
 
     LOCAL_ROOT = os.path.abspath("data") + os.sep
-
-    auth = tweepy.OAuthHandler(os.environ["KEY"], os.environ["KEY_SECRET"])
-    auth.set_access_token(os.environ["TOKEN"], os.environ["TOKEN_SECRET"])
-
-    #auth = tweepy.AppAuthHandler(os.environ['API_KEY'], os.environ['API_SECRET'])
-
-    api = tweepy.API(auth)
     df = pd.read_csv(LOCAL_ROOT + "twitter_users.csv")
     user_ids = df["id"].apply(str).to_list()
 
+    auth = tweepy.OAuthHandler(os.environ["KEY"], os.environ["KEY_SECRET"])
+    auth.set_access_token(os.environ["TOKEN"], os.environ["TOKEN_SECRET"])
+    api = tweepy.API(auth)
+
     while True:
         try:
-            myStreamListener = TweetStreamListener(tweepy_api=api, kafka_topic=args.topic)
-            myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-            myStream.filter(follow=user_ids[:50], languages=['en'])
+            tweetStreamListener = tweepy.Stream(auth=api.auth, listener=TweetStreamListener(tweepy_api=api))
+            tweetStreamListener.filter(follow=user_ids[:50], languages=['en'])
         except IncompleteRead:
             # Oh well, reconnect and keep trucking
             continue
         except KeyboardInterrupt:
             # Or however you want to exit this loop
-            myStream.disconnect()
+            tweetStreamListener.disconnect()
             break
 
 
